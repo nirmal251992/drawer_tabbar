8b6b3761de6292c0d1bd7ceea99672446990ed74
nirmal <nirmal251992@gmail.com>
2020-10-22 17:03:59 +0530

    dreawer and tabbar

--- lib/about.dart
+++ lib/about.dart
@@ -28,7 +28,6 @@ class About extends StatelessWidget {
               }
             ),
         ],
-
         ),
       ),
     );
--- lib/info.dart
+++ lib/info.dart
@@ -26,7 +26,6 @@ class Info extends StatelessWidget {
                 }
             ),
           ],
-
         ),
       ),
     );
